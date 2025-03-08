7694508c72598147a481b2b757e2b485b6b549da
Mariajose Franco Orozco <mariajosefranco@Mariajoses-MacBook-Pro.local>
2025-03-08 09:05:13 +0100

    format

--- filter_ranking.py
+++ filter_ranking.py
@@ -7,7 +7,8 @@ stop_words = STOP_WORDS
 
 def ranking(query: str, df: pd.DataFrame) -> pd.DataFrame:
     """
-    Filter the dataframe based on the query and return the modified dataframe with a ternary ranking system:
+    Filter the dataframe based on the query and return the modified dataframe with
+    a ternary ranking system:
     - If the query is in the text, match almost completely -> score = 1
     - If the query is partially in the text -> score = 2
     - If the query is not in the text, no match -> score = 3
@@ -24,11 +25,12 @@ def ranking(query: str, df: pd.DataFrame) -> pd.DataFrame:
         text = text.replace(',', ' ')
         # Remove stopwords using spaCy's list
         text = ' '.join([word for word in text.split() if word not in stop_words])
-        
+
         text_words = set(text.split())
         query_words = set(q.split())
-        
-        # Exact match: all words from the query are present in the text (regardless of order or position)
+
+        # Exact match: all words from the query are present in the text
+        # (regardless of order or position)
         if query_words.issubset(text_words):
             return 1
         # Partial match: at least one word from the query is in the text
@@ -37,7 +39,7 @@ def ranking(query: str, df: pd.DataFrame) -> pd.DataFrame:
         # No match: none of the words are present
         else:
             return 3
-    
+
     df['score'] = df['text'].apply(compute_score)
     df = df.sort_values(by='score')
     return df
